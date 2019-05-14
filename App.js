475a0c8acc1f8202a1aecd33f3c38b4d9a4d947e
Leonrdarc <leonardoarchila@hotmail.com>
2019-05-14 09:19:06 -0500

    Socket.io installed

--- App.js
+++ App.js
@@ -10,15 +10,22 @@ import React, {Component} from 'react';
 import {StyleSheet, Text, View, PermissionsAndroid, Alert, PropTypes, TouchableOpacity} from 'react-native';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import LoginScreen from './screens/LoginScreen';
+import SocketIOClient from 'socket.io-client';
+
 
 import DrawerNavigation from './navigation/DrawerNavigation'
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props){
+    super(props);
+
+    this.socket = SocketIOClient('http://localhost:3000');
+  }
   render() {
     return (
       <AppContainer/>
-
+      
       
     );
   }
