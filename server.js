8d0581200d776f7aa1fb2864c182f8a57748e5f4
Juan Pablo Rambal Heras <herasj@uninorte.edu.co>
2019-05-20 20:53:08 -0500

    Funciones

--- server.js
+++ server.js
@@ -7,9 +7,12 @@ const fetch = require('node-fetch')
 const jsonwebtoken = require('jsonwebtoken')
 const crypto = require('crypto');
 var socketIoJwtAuth = require('socketio-jwt-auth');
+var ClusterCon = "mongodb+srv://admin:contra12345@colectivos-hgrqm.mongodb.net/test?retryWrites=true";
 app.use(boom());
 server.listen(3000);
-
+const loc = io.of('/Location');
+//const socket = io('/Elnsp');
+//socket.join('some room');
 /**
  * Environment Variables:
  * In a real scenario get them using process.env
@@ -77,6 +80,8 @@ io.on('connection', function(socket) {
         message: 'success logged in!',
         user: socket.request.user
     });
+
+    
 });
 // io.on('connection', function (socket) {
 //     socket.on('AddData', (data) => {
@@ -121,3 +126,212 @@ async function getFacebookMe(token) {
     return res.json()
 }
 
+function AddUser(data) { //Parametros de entrada
+  //Retorna 0 si hubo error de conexión
+  //Retorna 1 si el procedimiento se realizó correctamente
+  //Retorna 2 si se encontró el mismo telefono
+  var MongoClient = require('mongodb').MongoClient;
+  //DbConnection
+  //mongodb://localhost:27017
+
+  MongoClient.connect(ClusterCon, function (err, client) {
+      //Mongodb Cluster URL
+      if (err) {
+          console.log("Connection Failed :C")
+          console.log(err);
+          return 0;
+      }
+      else {
+          var db = client.db('ColectivosDB'); //Base de datos objetivo
+
+          console.log("Connected to db");
+
+          //StartQuerys
+          db.collection('users', function (err, collection) { //Insert
+              collection.insert({
+                 FBID:data.id,
+                 name:data.name,
+                 lastname:data.lastname,
+                 birthday:data.birthday,
+                 phone:data.phone,
+                 country_code:data.cc,
+                 isDriver:data.isdriver
+              });
+              console.log('Success !')
+              db.collection('users').count(function (err, count) {
+                  if (err) throw err;
+
+                  console.log('Total Rows: ' + count);
+                  client.close;
+                  return 1;
+              });
+          });
+
+
+      }
+  });
+
+}
+
+function UpdateLocation(data){ //Parametros de entrada
+  //Retorna 0 si hubo error de conexión
+  //Retorna 1 si el procedimiento se realizó correctamente
+  //Retorna 2 si se encontró el mismo telefono
+  var MongoClient = require('mongodb').MongoClient;
+  //DbConnection
+  //mongodb://localhost:27017
+  //mongodb+srv://admin:contra12345@colectivos-hgrqm.mongodb.net/test?retryWrites=true
+  console.log('Intentando actualizar ')
+  MongoClient.connect(ClusterCon, function (err, client) {
+                      //Mongodb Cluster URL
+      if(err)
+      {
+          console.log("Connection Failed :C")
+          console.log(err);
+          return 0;
+      }
+      else
+      {
+          var db= client.db('ColectivosDB'); //Base de datos objetivo
+          //var ObjectID = require('mongodb').ObjectID; Search by ObjectID
+          console.log("Connected to db"); 
+          //StartQuerys
+          try {
+          db.collection('DriverLoc', function (err, collection) { //Insert
+              collection.updateOne(
+                  {cellphone: data.cel},
+                  {$set: {latitude: data.lat,longitude: data.lon}}
+              )                   
+          });
+      } catch (e) {
+          console.log(e);
+       }
+                
+                  
+  }});
+  
+}
+
+function GetLocation(id){
+  var MongoClient = require('mongodb').MongoClient;
+  console.log('Intentando actualizar ')
+  MongoClient.connect(ClusterCon, function (err, client) {
+                      //Mongodb Cluster URL
+      if(err)
+      {
+          console.log("Connection Failed :C")
+          console.log(err);
+          return 0;
+      }
+      else
+      {
+          var db= client.db('ColectivosDB'); //Base de datos objetivo
+          //var ObjectID = require('mongodb').ObjectID; Search by ObjectID
+          console.log("Connected to db"); 
+          //StartQuerys
+          try {
+              dbo.collection("DriverLoc").findOne({FBID: id}, function(err, result) {
+                  if (err) throw err;
+                  return result;
+                  db.close();
+                }); 
+      } catch (e) {
+          console.log(e);
+       }
+                
+                  
+  }});
+}
+
+function GetPeople(id){
+      var MongoClient = require('mongodb').MongoClient;
+      console.log('Intentando actualizar ')
+      MongoClient.connect(ClusterCon, function (err, client) {
+                          //Mongodb Cluster URL
+          if(err)
+          {
+              console.log("Connection Failed :C")
+              console.log(err);
+              return 0;
+          }
+          else
+          {
+              var db= client.db('ColectivosDB'); //Base de datos objetivo
+              //var ObjectID = require('mongodb').ObjectID; Search by ObjectID
+              console.log("Connected to db"); 
+              //StartQuerys
+              try {
+                  dbo.collection("People").findOne({FBID: id}, function(err, result) {
+                      if (err) throw err;
+                      return result;
+                      db.close();
+                    }); 
+          } catch (e) {
+              console.log(e);
+           }
+                    
+                      
+      }});
+}
+
+function GetRoute(id){
+  var MongoClient = require('mongodb').MongoClient;
+  console.log('Intentando actualizar ')
+  MongoClient.connect(ClusterCon, function (err, client) {
+                      //Mongodb Cluster URL
+      if(err)
+      {
+          console.log("Connection Failed :C")
+          console.log(err);
+          return 0;
+      }
+      else
+      {
+          var db= client.db('ColectivosDB'); //Base de datos objetivo
+          //var ObjectID = require('mongodb').ObjectID; Search by ObjectID
+          console.log("Connected to db"); 
+          //StartQuerys
+          try {
+              dbo.collection("Routes").findOne({routeID: id}, function(err, result) {
+                  if (err) throw err;
+                  return result;
+                  db.close();
+                }); 
+      } catch (e) {
+          console.log(e);
+       }
+                
+                  
+  }});
+}
+
+function GetAsgR (id){
+  var MongoClient = require('mongodb').MongoClient;
+  console.log('Intentando actualizar ')
+  MongoClient.connect(ClusterCon, function (err, client) {
+                      //Mongodb Cluster URL
+      if(err)
+      {
+          console.log("Connection Failed :C")
+          console.log(err);
+          return 0;
+      }
+      else
+      {
+          var db= client.db('ColectivosDB'); //Base de datos objetivo
+          //var ObjectID = require('mongodb').ObjectID; Search by ObjectID
+          console.log("Connected to db"); 
+          //StartQuerys
+          try {
+              dbo.collection("Routes").findOne({routeID: id}, function(err, result) {
+                  if (err) throw err;
+                  return result;
+                  db.close();
+                }); 
+      } catch (e) {
+          console.log(e);
+       }
+                
+                  
+  }});
+}
